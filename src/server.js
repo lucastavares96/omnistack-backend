7fcac30fda6a34b8a8e53d7b634c473642cb13bf
Lucas Tavares <lucasstavares96@gmail.com>
2019-04-11 02:08:44 -0300

    Adicionadas variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -34,5 +34,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
 
